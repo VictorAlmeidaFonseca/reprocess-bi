01c23461a9b3435e05201586221dfcd094ceda81
VictorAlmeidaFonseca <fonseca.almeida.victor@gmail.com>
2021-01-13 10:16:26 -0300

    ajust vetx parmas

--- src/config/vtex-params.js
+++ src/config/vtex-params.js
@@ -2,5 +2,5 @@ const moment = require('moment')
 
 module.exports = { 
   "f_creationDate": 
-  `creationDate:[${moment(process.env.START_DATE).format()} TO ${moment(process.env.END_DATE).format()}]`
+  `creationDate:[${moment(new Date(process.env.START_DATE)).format()} TO ${moment(new Date(process.env.END_DATE)).format()}]`
 }
